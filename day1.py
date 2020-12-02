a5ffc0b9fafabab3416d0a7dd2d5d33ae812c24f
suchotomas <sucho.tomas@gmail.com>
2020-12-02 02:07:00 +0100

    code reformat

--- day1.py
+++ day1.py
@@ -1,28 +1,28 @@
 import numpy as np
 import time
-list = [1721, 979,366,299,675, 1456]
-list = np.loadtxt('day1.txt')
-list = sorted(list, reverse=True)
-list = np.array(list, dtype=np.int)
+# _list = [1721, 979, 366, 299, 675, 1456]
+_list = np.loadtxt('day1.txt', dtype=int)
+_list = np.flip(np.sort(_list))
 
-print(f'size of list = {len(list)}')
-year = 2020
-counter = 0
-ts = time.time()
-# print(list)
+print(f'size of the list = {len(_list)}')
+
 def skip(idx):
-    if idx > 2020 - list[-1] - list[-2]:
+    if idx > 2020 - _list[-1] - _list[-2]:
         return True
     return False
 
 done = False
-for idx in list:
+year = 2020
+counter = 0
+ts = time.time()
+
+for idx in _list:
     if skip(idx):
         continue
-    for idy in list:
+    for idy in _list:
         if skip(idy):
             continue
-        for idz in list:
+        for idz in _list:
             if skip(idz):
                 continue
             counter +=1
@@ -30,8 +30,7 @@ for idx in list:
                 if not done:
                     print(f'{idx} * {idy} * {idz} = {idx*idy*idz}')
                     done = True
-print(f'\nnumber of opearations = {counter}')
+                    #exit()
+
+print(f'\nCounter = {counter}')
 print(f'Whole process: {time.time()-ts :.02f} sec')
-
-
-
